f3f3cc3a4e32d9143509b8cf06328f5bbc3c0a95
Sebastian Hoang <shoang6702@gmail.com>
2023-10-23 10:48:44 -1000

    workingish 2.5

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 const inquirer =require('inquirer')
 // TODO: Create an array of questions for user input
-const questions = ['Title', 'Description', 'Table of Contents', 'Installation', 'Usage', 'Credit', 'License', 'Contributing', 'Test', 'Questions'];
+const questions = ['#Title', 'Description', 'Table of Contents', 'Installation', 'Usage', 'Credit', 'License', 'Contributing', 'Test', 'Questions'];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
@@ -13,48 +13,49 @@ inquirer
   .prompt([
     {
       type: 'Title',
-      name: 'title',
+      name: '#Title',
       message: 'What is the title?',
     },
     {
       type: 'Description',
-      name: 'description',
+      name: '##Description',
       message: 'Describe the application?',
     },
     {
         type: 'Table of Contents',
-        name: 'tableofcontents',
+        name: '##Table of Contents',
         message: 'What is included?',
     },
     {
         type: 'Installation',
-        name: 'installation',
+        name: '##Installation',
         message: 'How do you install?',
     },
     {
         type: 'Usage',
-        name: 'usage',
-        message: 'What are the terms for using the application??',
+        name: '##Usage',
+        message: 'What are the terms for using the application?',
     },
     {
         type: 'License',
-        name: 'license',
         message: 'What kind of license is required?',
+        name: '##License',
+        choices: ['None', 'Unlicensed', 'Open Software License 3.0', 'Creative Commons license family', 'European Union Public License 1.1']
     },
     {
         type: 'Contributing',
-        name: 'contributing',
+        name: '##Contributing',
         message: 'Who contributed to the project?',
     },
     {
         type: 'Test',
-        name: 'test',
+        name: '##Test',
         message: 'Test?',
     },
     {
         type: 'Questions',
-        name: 'questions',
-        message: 'Questions?',
+        name: '##Questions',
+        message: 'Github name and Email?',
     },
   ])
   .then((data) => {
@@ -82,23 +83,23 @@ init();
 
 // GIVEN a command-line application that accepts user input
 
-// WHEN I am prompted for information about my application repository ()
+// WHEN I am prompted for information about my application repository (Working)
 // THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
 
-// WHEN I enter my project title ()
+// WHEN I enter my project title (Working)
 // THEN this is displayed as the title of the README
 
-// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions ()
-// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions (Working)
+// THEN this information is adNoded to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
 
-// WHEN I choose a license for my application from a list of options ()
+// WHEN I choose a license for my application from a list of options (Semi Working)
 // THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
 
-// WHEN I enter my GitHub username ()
+// WHEN I enter my GitHub username (half working)
 // THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
 
-// WHEN I enter my email address ()
+// WHEN I enter my email address (half working)
 // THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
 
-// WHEN I click on the links in the Table of Contents ()
+// WHEN I click on the links in the Table of Contents (Not Working)
 // THEN I am taken to the corresponding section of the README
